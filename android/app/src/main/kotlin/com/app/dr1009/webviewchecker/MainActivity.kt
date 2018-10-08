4526794673e4dea38949c33371bc4bd07c009de0
Koji Wakamiya <koji.wakamiya@gmail.com>
2018-10-08 18:36:43 +0900

    Create Project

---
+++ android/app/src/main/kotlin/com/app/dr1009/webviewchecker/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.app.dr1009.webviewchecker
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity(): FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
