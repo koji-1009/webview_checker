ab5542b9ce0ea7f6c56ae8f7863a917cb2591d7e
Koji Wakamiya <koji.wakamiya@gmail.com>
2022-05-19 22:19:12 +0900

    Flutter 3

--- android/app/src/main/java/com/app/dr1009/webview_checker/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.app.dr1009.webview_checker;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/app/dr1009/webview_checker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.dr1009.webview_checker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
